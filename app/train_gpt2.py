4d747eb90ab13f4a60e43f70d7ad2aa5a434ba8f
yijunx <yijunx@umich.edu>
2024-06-13 02:21:31 +0000

    started

---
+++ app/train_gpt2.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384  # embedding dimensions
+
+
+class Block(nn.Module):
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        # self attention, this is reduce operation
+        self.attn = CasualSelfAttension(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        # multilayer perceptron, this is a map operation
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                # encoding layer for token
+                wte = nn.Embedding(config.vocab_size, config.n_embd),
+                # positional embedding layer
+                wpe = nn.Embedding(config.block_size, config.n_embd),
+                # hidden layer
+                h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                # layNorm, final
+                ln_f = nn.LayerNorm(config.n_embd)
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
